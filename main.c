2ad7c5e6fa8381044b36f905191ebe1719366bcb
temirovavika <vika.temirova.06@inbox.ru>
2024-08-21 15:49:41 +0300

    Add files via upload
    я скачала гитхаб

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <TXLib.h>
+
+int main(void)
+{
+    double firstcoef = 0;
+    double secondcoef = 0;
+    double thirdcoef = 0;
+    double diskr = 0;
+    double sol1 = 0;
+    double sol2 = 0;
+
+
+    printf("Введите коэффициенты квадратного уравнения:");
+    while (scanf("%lg %lg %lg", &firstcoef, &secondcoef, &thirdcoef) == 3)
+    {
+        diskr = secondcoef *  secondcoef - 4 * firstcoef * thirdcoef;
+
+        if (firstcoef == 0 && secondcoef == 0 && thirdcoef == 0)
+            printf("Бесконечное количество кореней\n");
+
+        else if (firstcoef == 0 && secondcoef != 0)
+            printf("Линейный случай корень равен: %lg \n", (-thirdcoef) / secondcoef);
+
+        else if (diskr > 0)
+        {
+            sol1 = ((-secondcoef) + sqrt(diskr)) / 2 * firstcoef;
+            sol2 = ((-secondcoef) - sqrt(diskr)) / 2 * firstcoef;
+            printf("Корни уравнения равны соответсвенно %lg и %lg \n", sol1, sol2);
+        }
+
+        else if (diskr == 0)
+        {
+            sol1 = (-secondcoef) / 2 * firstcoef;
+            sol2 = sol1;
+            printf("Единственный корень равен %lg\n",sol1);
+        }
+
+        else
+            printf("Корней нет\n");
+
+        printf("Введите слудующие значения:");
+
+    }
+    return 0;
+}
