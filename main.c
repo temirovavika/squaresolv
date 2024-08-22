25317a7e1a1bb39ff844d7f26a0b8b17ccb63852
temirovavika <vika.temirova.06@inbox.ru>
2024-08-22 20:31:03 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,47 +1,14 @@
 #include <TXLib.h>
 
 int squaresol(double coef_a, double coef_b, double therdcoef, double *sol1, double *sol2 );
+int vvod(void);
+
+
 int main(void)
 {
-    double coef_a = 0;
-    double coef_b = 0;
-    double coef_c = 0;
-    double sol1   = 0;
-    double sol2   = 0;
-    int kolvo     = 0;
-    int save_scanf = 0;
-
-    printf("Введите коэффициенты квадратного уравнения:");
-
-    while (scanf("%lg %lg %lg", &coef_a, &coef_b, &coef_c) != 3)
-    {
-        while(getchar() != '\n') {};
-        printf("Введите другие значения:\n");
-    }
 
 
-
-         kolvo = squaresol(coef_a, coef_b, coef_c, &sol1, &sol2);
-
-            switch (kolvo)
-            {
-                case -1: printf("Бесконечное количество корней\n");
-                     break;
-                case  0: printf("Корней нет\n");
-                     break;
-                case  1: printf("Количество корней:%d корень равен: %lg\n", kolvo, sol1);
-                     break;
-                case  2: printf("Количество корней:%d корни равны соответсвенно: %lg, %lg\n",kolvo, sol1, sol2);
-                     break;
-                default: printf("# Ошибка!!! Необычное количество корней\n");
-                     break;
-
-            }
-
-            printf("Введите следующие значения: ");
-
-
-
+    while(vvod() == 1) { }
 
     return 0;
 }
@@ -94,4 +61,40 @@ int squaresol( double coef_a, double coef_b,  double coef_c, double *sol1, doubl
 
 
 
+}
+
+int  vvod(void)
+{
+    double coef_a = 0;
+    double coef_b = 0;
+    double coef_c = 0;
+    double sol1   = 0;
+    double sol2   = 0;
+    int kolvo     = 0;
+    int save_scanf = 0;
+
+    printf("Введите коэффициенты квадратного уравнения:");
+    while(scanf("%lg %lg %lg", &coef_a, &coef_b, &coef_c ) != 3)
+    {
+     while(getchar() != '\n') {};
+     printf("Введите другие значения:");
+    }
+
+      kolvo = squaresol(coef_a, coef_b, coef_c, &sol1, &sol2);
+
+            switch (kolvo)
+            {
+                case -1: printf("Бесконечное количество корней\n");
+                     break;
+                case  0: printf("Корней нет\n");
+                     break;
+                case  1: printf("Количество корней:%d корень равен: %lg\n", kolvo, sol1);
+                     break;
+                case  2: printf("Количество корней:%d корни равны соответсвенно: %lg, %lg\n",kolvo, sol1, sol2);
+                     break;
+                default: printf("# Ошибка!!! Необычное количество корней\n");
+                     break;
+
+            }
+     return 1;
 }
