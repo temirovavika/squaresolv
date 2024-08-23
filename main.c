2bc8f20235b6fb1ae6cfe179e131cdd71fcb7bb1
temirovavika <vika.temirova.06@inbox.ru>
2024-08-23 15:18:15 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -19,60 +19,13 @@ int squaresol( double coef_a, double coef_b,  double coef_c, double *sol1, doubl
 {
     if (coef_a != 0)
     {
-        if (case_square(coef_a, coef_b, coef_c, sol1, sol2 ) == 1)
-            return 1;
-        else if (case_square(coef_a, coef_b,  coef_c, sol1, sol2) == 2)
-            return 2;
-        else
-            return 0;
-        /*// quadratic
-        double diskr = coef_b *  coef_b - 4 * coef_a * coef_c;
-        if (diskr == 0)
-        {
-            *sol1 = -coef_b / (2 * coef_a);
-            *sol2 = *sol1;
-            return 1;
-        }
-        else if (diskr > 0)
-        {
-            *sol1 = (-coef_b + sqrtf(diskr)) / (2 * coef_a);
-            *sol2 = (-coef_b - sqrtf(diskr)) / (2 * coef_a);
-            return 2;
-        }
-        else
-        {
-            return 0;
-        } */
+        return case_square(coef_a, coef_b, coef_c, sol1, sol2);
     }
+
     else
     {
-        if (case_linear(coef_b, coef_c, sol1) == -1)
-            return -1;
-        else if (case_linear(coef_b, coef_c, sol1) == 0)
-            return 0;
-        else
-            return 1;
-        // linear
-        /*if(coef_b == 0)
-        {
-            if(coef_c == 0)
-            {
-                return -1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        else
-        {
-            *sol1 = -coef_c / coef_b;
-            return 1;
-        } */
+        return case_linear(coef_b, coef_c, sol1);
     }
-
-
-
 }
 
 int  report(void)
