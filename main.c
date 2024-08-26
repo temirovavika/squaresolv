4f5052e79317923c4b917220392d15d8137b059b
temirovavika <vika.temirova.06@inbox.ru>
2024-08-26 19:44:42 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <TXLib.h>
 
-int squaresol(double coef_a, double coef_b, double coef_c);
+int squaresol(double coef_a, double coef_b, double coef_c, double *sol1, double *sol2);
 int report(void);
 void input(double *coef_a, double *coef_b, double *coef_c );
 void output( int kolvo, double sol1, double sol2);
 int case_square(double coef_a, double coef_b, double coef_c, double *sol1, double *sol2);
-int case_linear(double coef_b, double coef_c, double *sol1);
+int case_linear(double coef_b, double coef_c, double *sol1, double *sol2);
+int Un_test1( double coef_a, double coef_b, double coef_c, double sol1ex, double sol2ex, int kolvoex);
+int dablsr(double sol1, double sol2);
+int nansr(double sol1, double sol2);
 int main(void)
 {
+    Un_test1(1, -5, 6, 3, 2, 2);
+    Un_test1(1, -12, 36, 6, NAN, 1);
+    Un_test1(0, 5, -10, 2, NAN, 1);
+    Un_test1(0, 0, 0, NAN, NAN, -1);
+    Un_test1(2, 3, 4, NAN, NAN, 0);
+    Un_test1(0, 0, 4, NAN, NAN, 0);
 
 
     while(report() == 1) { }
@@ -17,6 +26,8 @@ int main(void)
 
 int squaresol( double coef_a, double coef_b,  double coef_c, double *sol1, double *sol2)
 {
+    assert(sol1 != NULL);
+    assert(sol2 != NULL);
     if (coef_a != 0)
     {
         return case_square(coef_a, coef_b, coef_c, sol1, sol2);
@@ -24,7 +35,7 @@ int squaresol( double coef_a, double coef_b,  double coef_c, double *sol1, doubl
 
     else
     {
-        return case_linear(coef_b, coef_c, sol1);
+        return case_linear(coef_b, coef_c, sol1, sol2);
     }
 }
 
@@ -46,8 +57,11 @@ int  report(void)
 
 void input( double *coef_a, double *coef_b, double *coef_c)
 {
+  assert(coef_a != NULL);
+  assert(coef_b != NULL);
+  assert(coef_c != NULL);
 
-printf("Введите коэффициенты квадратного уравнения:");
+ printf("Введите коэффициенты квадратного уравнения:");
     while(scanf("%lg %lg %lg", coef_a, coef_b, coef_c ) != 3)
     {
      while(getchar() != '\n') {};
@@ -75,11 +89,13 @@ void output(int kolvo, double sol1, double sol2)
 
 int case_square( double coef_a, double coef_b,  double coef_c, double *sol1, double *sol2)
 {
+    assert(sol1 != NULL);
+    assert(sol2 != NULL);
     double diskr = coef_b *  coef_b - 4 * coef_a * coef_c;
         if (diskr == 0)
         {
             *sol1 = -coef_b / (2 * coef_a);
-            *sol2 = *sol1;
+            *sol2 = NAN;
             return 1;
         }
         else if (diskr > 0)
@@ -90,27 +106,79 @@ int case_square( double coef_a, double coef_b,  double coef_c, double *sol1, dou
         }
         else
         {
+            *sol1 = NAN;
+            *sol2 = NAN;
+
             return 0;
         }
 
 }
 
-int case_linear(double coef_b, double coef_c, double *sol1)
+int case_linear(double coef_b, double coef_c, double *sol1, double *sol2)
 {
+  assert(sol1 != NULL);
   if(coef_b == 0)
-        {
-            if(coef_c == 0)
-            {
-                return -1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
+  {
+    if(coef_c == 0)
+    {
+     *sol1 = NAN;
+     *sol2 = NAN;
+     return -1;
+    }
     else
-        {
-            *sol1 = -coef_c / coef_b;
-            return 1;
-        }
+    {
+     *sol1 = NAN;
+     *sol2 = NAN;
+     return 0;
+    }
+  }
+  else
+  {
+    *sol1 = -coef_c / coef_b;
+    *sol2 = NAN;
+    return 1;
+  }
 }
+
+int Un_test1(double coef_a, double coef_b, double coef_c, double sol1ex, double sol2ex, int kolvoex)
+{
+ double sol1 = 0;
+ double sol2 = 0;
+ int kolvo = squaresol(coef_a, coef_b, coef_c, &sol1, &sol2);
+
+ if(!nansr(sol1, sol1ex) || !nansr(sol2, sol2ex) || kolvo != kolvoex)
+ printf("Ошибка Test1: coef_a = %lg; coef_b = %lg, coef_c = %lg, sol1 = %lg, sol2 = %lg, kolvo = %d\n"
+ "ожидалось: sol1 = %lg, sol2 = %lg, kolvo = %d\n",
+ coef_a, coef_b, coef_c, sol1, sol2, kolvo, sol1ex, sol2ex, kolvoex);
+
+ return 1;
+}
+int dablsr(double sol1, double sol2)
+{
+    const double p = 0.000000000001;
+    if (fabs(sol1 - sol2) < p)
+        return 1;
+    else
+        return 0;
+}
+
+int nansr(double sol1, double sol2)
+{
+        if ((isnan(sol1) && isnan(sol2))
+            || (dablsr(sol1, sol2) == 1))
+            return 1;
+        else
+            return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
